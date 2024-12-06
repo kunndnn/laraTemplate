0932e65ccfc88f678a18ced469c196c3b32a3a46
kunndnn <44980639+kunndnn@users.noreply.github.com>
2024-12-06 23:40:44 +0530

    optimizations

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -43,9 +43,8 @@ class AuthController extends Controller
             'password.regex' => 'The password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one numeric digit, and one special character.',
         ]);
 
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
+        
 
         $otp = env('OTP', mt_rand(1000, 9999));
 
@@ -59,9 +58,7 @@ class AuthController extends Controller
         $user->device_type = $request->device_type;
         $user->device_token = $request->device_token;
 
-        if (isset($request->device_model)) {
-            $user->device_model = $request->device_model;
-        }
+        if (isset($request->device_model)) $user->device_model = $request->device_model;
 
         if ($request->profile_image) {
             $file = $request->file('profile_image');
@@ -74,9 +71,9 @@ class AuthController extends Controller
         $userName = $request->get('first_name');
         $templatePath = 'templates.emails.welcome';
         $subject = 'Welcome Plateform OTP!';
-        if (!env('OTP')) {
-            $this->sendEmail($request->get('email'), $subject, $templatePath, $otp, $userName);
-        }
+
+        if (!env('OTP')) $this->sendEmail($request->get('email'), $subject, $templatePath, $otp, $userName);
+
         return $this->successRes(201, 'User register successfully!', $user);
     }
 
@@ -87,22 +84,19 @@ class AuthController extends Controller
             'otp' => 'required'
         ]);
 
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
+        
 
         $check_user  = User::where('email', $request->email)->where('otp', $request->otp)->first();
-        if ($check_user) {
-            $now = now();
-            $accessToken   = $check_user->createToken('authToken')->plainTextToken;
-            User::where('email', $request->email)->update(['is_verified' => 1, 'email_verified_at' => $now, 'otp' => null, 'remember_token' => $accessToken]);
-            $user =  User::find($check_user->id);
-            $user['token'] = $accessToken;
 
-            return $this->successRes(200, 'OTP verified successfully!', $user);
-        } else {
-            return $this->errorRes(400, 'Invalid OTP');
-        }
+        if (!$check_user) return $this->errorRes(400, 'Invalid OTP');
+
+        $now = now();
+        $accessToken   = $check_user->createToken('authToken')->plainTextToken;
+        User::where('email', $request->email)->update(['is_verified' => 1, 'email_verified_at' => $now, 'otp' => null, 'remember_token' => $accessToken]);
+        $user =  User::find($check_user->id);
+        $user['token'] = $accessToken;
+        return $this->successRes(200, 'OTP verified successfully!', $user);
     }
 
     public function resendOtp(Request $request)
@@ -110,24 +104,22 @@ class AuthController extends Controller
         $validator = Validator::make($request->all(), [
             'email' => 'required|email|exists:users,email',
         ]);
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
+        
 
         $otp = env('OTP', mt_rand(1000, 9999));
         $user = User::where('email', $request->email)->update(['otp' => $otp]);
-        if ($user) {
-            $user = User::where('email', $request->email)->first();
-            $userName = $user->first_name;
-            $templatePath = 'templates.emails.resend_otp';
-            $subject = 'Resend OTP!';
-            if (!env('OTP')) {
-                $this->sendEmail($user->email, $subject, $templatePath, $otp, $userName);
-            }
-            return $this->successRes(200, 'OTP resend successfully!', $otp);
-        } else {
-            return $this->errorRes(400, 'Email does not exist!');
-        }
+
+        if (!$user) return $this->errorRes(400, 'Email does not exist!');
+
+        $user = User::where('email', $request->email)->first();
+        $userName = $user->first_name;
+        $templatePath = 'templates.emails.resend_otp';
+        $subject = 'Resend OTP!';
+
+        if (!env('OTP')) $this->sendEmail($user->email, $subject, $templatePath, $otp, $userName);
+
+        return $this->successRes(200, 'OTP resend successfully!', $otp);
     }
 
     public function login(Request $request)
@@ -139,99 +131,81 @@ class AuthController extends Controller
             'device_token' => 'nullable',
             'device_model' => 'nullable',
         ]);
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'status' => 1])) {
-            $user_detail = User::where('email', $request->email)->first();
-            if ($user_detail->is_verified == 1) {
-                $user_detail->device_type  = $request->device_type;
-                $user_detail->device_token = $request->device_token;
-                $user_detail->device_model = $request->device_model;
-                $user_detail->is_online = 1;
-                $user_detail->last_login =  now();
-                $user_detail->save();
+        if ($validator->fails()) return $this->errorHandle($validator);
 
-                $token = $user_detail->createToken('authToken')->plainTextToken;
-                $user_detail->remember_token = $token;
-                $user_detail->save();
-
-                $user_detail = User::where('email', $request->email)->first();
-                $user_detail->token = $token;
-
-                return $this->successRes(200, 'User Login Successfully', $user_detail);
-            } else {
-                $otp = env('OTP', mt_rand(1000, 9999));
-                $user_detail->otp = $otp;
-                $user_detail->device_type = $request->device_type;
-                $user_detail->device_token = $request->device_token;
-                $user_detail->device_model = $request->device_model;
-                $user_detail->save();
-
-                $userName = $user_detail->first_name;
-                $templatePath = 'templates.emails.resend_otp';
-                $subject  = 'Resend OTP!';
-                if (!env('OTP')) {
-                    $this->sendEmail($user_detail->email, $subject, $templatePath, $otp, $userName);
-                }
-                return $this->errorRes(200, 'Please verify your account before login!', $user_detail);
-            }
-        } else {
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password, 'status' => 1])) {
             return $this->errorRes(400, 'Invalid Credentials');
+        }
+        $user_detail = User::where('email', $request->email)->first();
+        if ($user_detail->is_verified == 1) {
+            $user_detail->device_type  = $request->device_type;
+            $user_detail->device_token = $request->device_token;
+            $user_detail->device_model = $request->device_model;
+            $user_detail->is_online = 1;
+            $user_detail->last_login =  now();
+            $user_detail->save();
+
+            $token = $user_detail->createToken('authToken')->plainTextToken;
+            $user_detail->remember_token = $token;
+            $user_detail->save();
+
+            $user_detail = User::where('email', $request->email)->first();
+            $user_detail->token = $token;
+
+            return $this->successRes(200, 'User Login Successfully', $user_detail);
+        } else {
+            $otp = env('OTP', mt_rand(1000, 9999));
+            $user_detail->otp = $otp;
+            $user_detail->device_type = $request->device_type;
+            $user_detail->device_token = $request->device_token;
+            $user_detail->device_model = $request->device_model;
+            $user_detail->save();
+
+            $userName = $user_detail->first_name;
+            $templatePath = 'templates.emails.resend_otp';
+            $subject  = 'Resend OTP!';
+            if (!env('OTP')) $this->sendEmail($user_detail->email, $subject, $templatePath, $otp, $userName);
+            return $this->errorRes(200, 'Please verify your account before login!', $user_detail);
         }
     }
 
     public function getProfile(Request $request)
     {
         $user = User::find(Auth::id());
-        if ($user) {
-            return $this->successRes(200, 'User profile fetched successfully', $user);
-        } else {
-            return $this->errorRes(404, 'User not found');
-        }
+        if (!$user) return $this->errorRes(404, 'User not found');
+        return $this->successRes(200, 'User profile fetched successfully', $user);
     }
 
     public function editProfile(Request $request)
     {
         $user = User::where("id", Auth::id())->first();
-        if ($user) {
-            if (isset($request->first_name)) {
-                $user->first_name = $request->first_name;
-            }
-            if (isset($request->last_name)) {
-                $user->last_name = $request->last_name;
-            }
-            if (isset($request->phone)) {
-                $user->phone = $request->phone;
-            }
-            if ($request->has('email')) {
-                $check = User::where('id', '!=', Auth::id())
-                    ->where("email", $request->email)
-                    ->first();
+        if (!$user) return $this->errorRes(404, 'User not found');
+        if (isset($request->first_name)) $user->first_name = $request->first_name;
+        if (isset($request->last_name)) $user->last_name = $request->last_name;
+        if (isset($request->phone)) $user->phone = $request->phone;
 
-                if ($check) {
-                    return $this->errorRes(400, 'Email already exists');
-                }
-                $user->email = $request->email;
-            }
-            if (isset($request->profile_image)) {
+        if ($request->has('email')) {
+            $check = User::where('id', '!=', Auth::id())
+                ->where("email", $request->email)
+                ->first();
 
-                $imagePath = public_path('uploads/') . $user->profile_image;
-                if (File::exists($imagePath)) {
-                    File::delete($imagePath);
-                }
-
-                $file = $request->file('profile_image');
-                $name = time() . "." . $file->getClientOriginalExtension();
-                $file->move(public_path('uploads'), $name);
-                $user->profile_image = $name;
-            }
-
-            $user->save();
-            return $this->successRes(200, 'Profile edit successfully', $user);
-        } else {
-            return $this->errorRes(404, 'User not found');
+            if ($check) return $this->errorRes(400, 'Email already exists');
+            $user->email = $request->email;
         }
+        if (isset($request->profile_image)) {
+
+            $imagePath = public_path('uploads/') . $user->profile_image;
+            if (File::exists($imagePath)) {
+                File::delete($imagePath);
+            }
+
+            $file = $request->file('profile_image');
+            $name = time() . "." . $file->getClientOriginalExtension();
+            $file->move(public_path('uploads'), $name);
+            $user->profile_image = $name;
+        }
+        $user->save();
+        return $this->successRes(200, 'Profile edit successfully', $user);
     }
 
     public function changePassword(Request $request)
@@ -240,18 +214,15 @@ class AuthController extends Controller
             'old_password' => 'required',
             'new_password' => 'required'
         ]);
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
+        
         $check = Hash::check($request->old_password, Auth::user()->password);
-        if ($check) {
-            $user = User::where('id', Auth::id())->first();
-            $user->password = bcrypt($request->new_password);
-            $user->save();
-            return $this->successRes(200, 'Password changed successfully', $user);
-        } else {
-            return $this->errorRes(400, 'Incorrect old password');
-        }
+        if (!$check) return $this->errorRes(400, 'Incorrect old password');
+
+        $user = User::where('id', Auth::id())->first();
+        $user->password = bcrypt($request->new_password);
+        $user->save();
+        return $this->successRes(200, 'Password changed successfully', $user);
     }
 
     public function forgotPassword(Request $request)
@@ -260,23 +231,20 @@ class AuthController extends Controller
             'email' => 'required|email|exists:users,email',
         ]);
 
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
+        
         $otp = env('OTP', mt_rand(1000, 9999));
         $user = User::where('email', $request->email)->update(['otp' => $otp]);
-        if ($user) {
-            $user = User::where('email', $request->email)->first();
-            $userName = $user->first_name;
-            $templatePath = 'templates.emails.forgot_password';
-            $subject  = 'Forgot Password OTP!';
-            if (!env('OTP')) {
-                $this->sendEmail($user->email, $subject, $templatePath, $otp, $userName);
-            }
-            return $this->successRes(201, 'OTP sent successfully!', $otp);
-        } else {
-            return $this->errorRes(400, 'Email does not exist!');
-        }
+        if (!$user) return $this->errorRes(400, 'Email does not exist!');
+
+        $user = User::where('email', $request->email)->first();
+        $userName = $user->first_name;
+        $templatePath = 'templates.emails.forgot_password';
+        $subject  = 'Forgot Password OTP!';
+
+        if (!env('OTP')) $this->sendEmail($user->email, $subject, $templatePath, $otp, $userName);
+
+        return $this->successRes(201, 'OTP sent successfully!', $otp);
     }
 
     public function resetPassword(Request $request)
@@ -286,19 +254,16 @@ class AuthController extends Controller
             'password' => 'required',
             'confirm_password' => 'required|same:password'
         ]);
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
+        
         $user  = User::where('email', $request->email)->first();
-        if ($user) {
-            $password = $request->get('password');
-            $user = User::where('email', $request->email)->first();
-            $user->password = bcrypt($password);
-            $user->save();
-            return $this->successRes(200, 'Password Change successfully!', $user);
-        } else {
-            return $this->errorRes(400, 'Email does not exist!');
-        }
+        if (! $user) return $this->errorRes(400, 'Email does not exist!');
+
+        $password = $request->get('password');
+        $user = User::where('email', $request->email)->first();
+        $user->password = bcrypt($password);
+        $user->save();
+        return $this->successRes(200, 'Password Change successfully!', $user);
     }
 
     public function logout(Request $request)
@@ -314,16 +279,14 @@ class AuthController extends Controller
     public function deleteAccount(Request $request)
     {
         $user = User::find(Auth::id());
-        if ($user) {
-            $imagePath = public_path('uploads/') . $user->profile_image;
-            if (File::exists($imagePath)) {
-                File::delete($imagePath);
-            }
-            $user->delete();
-            return $this->successRes(200, 'Account successfully deleted!');
-        } else {
-            return $this->errorRes(404, 'User not found');
+        if ($user) return $this->errorRes(404, 'User not found');
+
+        $imagePath = public_path('uploads/') . $user->profile_image;
+        if (File::exists($imagePath)) {
+            File::delete($imagePath);
         }
+        $user->delete();
+        return $this->successRes(200, 'Account successfully deleted!');
     }
 
     public function socialLogin(Request $request)
@@ -337,9 +300,7 @@ class AuthController extends Controller
             'device_model' => 'nullable',
         ]);
 
-        if ($validator->fails()) {
-            return $this->errorHandle($validator);
-        }
+        if ($validator->fails()) return $this->errorHandle($validator);
 
         $socialId = $request->socialId;
         $socialType = $request->socialType;
